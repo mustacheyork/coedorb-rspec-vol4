ea9811bf474fc3d593635a985b74b0651d245e91
Kanako Kobayashi <mustacheyork@gmail.com>
2015-04-19 10:26:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# coedorb-rspec-vol4
---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "users/new", :type => :view do
+  before(:each) do
+    assign(:user, User.new(
+      :name => "MyString",
+      :address => "MyString"
+    ))
+  end
+
+  it "renders new user form" do
+    render
+
+    assert_select "form[action=?][method=?]", users_path, "post" do
+
+      assert_select "input#user_name[name=?]", "user[name]"
+
+      assert_select "input#user_address[name=?]", "user[address]"
+    end
+  end
+end
